2ee4ad05ab026f52fa656fb45f8b00010d837c6f
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2025-10-17 16:37:34 +0900

    feat: 출력부 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,6 +8,11 @@ public class Calculator {
         System.out.println("덧셈할 문자열을 입력해 주세요.\n");
         String input = Console.readLine();
 
+        // 계산
+        int sum = 0;
+
+        // 출력
+        System.out.println("결과 : " + sum);
 
     }
 
