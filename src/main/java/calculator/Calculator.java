4e2e64ae13799003d5faa5515129481586a3aac4
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2025-10-20 16:57:26 +0900

    refactor: input, output 로직 분리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,20 +1,27 @@
 package calculator;
 
+import calculator.unit.InputUnit;
+import calculator.unit.OutputUnit;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Calculator {
+
     public void run() {
         // 입력
-        System.out.println("덧셈할 문자열을 입력해 주세요.\n");
-        String input = Console.readLine();
+        String input = InputUnit.input();
 
         // 계산
         int sum = 0;
 
         // 출력
-        System.out.println("결과 : " + sum);
+        OutputUnit.printResult(sum);
 
     }
+
+
+
+
+
 
 
 }
